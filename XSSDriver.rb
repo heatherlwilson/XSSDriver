a5c86ae133d9617b11fae2dddfba83b014c2b11e
Steve Campbell <31324675+scampbell-r7@users.noreply.github.com>
2018-07-18 14:05:59 -0400

    Update XSSDriver.rb

--- XSSDriver.rb
+++ XSSDriver.rb
@@ -11,11 +11,18 @@ if ARGV.empty?
   exit
 end
 
-
 # Example to enable use of a Socks proxy:
+# To enable, uncomment the following three lines and comment out line 27.
 # profile = Selenium::WebDriver::Firefox::Profile.new
 # profile.proxy = Selenium::WebDriver::Proxy.new socks: '127.0.0.1:9001'
 # browser = Watir::Browser.new :firefox, profile: profile
+
+# Example to enable use of Burp Proxy. 
+# If your site requires authentication, enable the cookie jar in Burp Suite Project Options for the proxy tool.
+# To enable, uncomment the following four lines and comment out line 27. 
+# profile = Selenium::WebDriver::Firefox::Profile.new
+# profile.proxy = Selenium::WebDriver::Proxy.new http: '127.0.0.1:8080', ssl: '127.0.0.1:8080'
+# browser = Watir::Browser.new :firefox, profile: profile, acceptInsecureCerts: true
 
 browser = Watir::Browser.new :firefox
 xssfile = ARGV[0]
